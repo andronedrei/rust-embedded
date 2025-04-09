e3e4e281abb897b1f4ca543256741f01e1012c7c
Andrei A. <119336147+andronedrei@users.noreply.github.com>
2025-04-09 15:34:31 +0300

    Lab05 modified

--- lab05/src/main.rs
+++ lab05/src/main.rs
@@ -1,89 +1,90 @@
-// +---------------------------------------------------------------------------+
-// |                             PM/MA lab skel                                |
-// +---------------------------------------------------------------------------+
-
-//! By default, this app prints a "Hello world" message with `defmt`.
-
 #![no_std]
 #![no_main]
-
-use core::cmp::{max, min};
+#![allow(unused_imports)]
 
 use embassy_executor::Spawner;
-use embassy_futures::{select::select, yield_now};
-// use embassy_net::Config; // not useful
-use embassy_time::{Duration, Instant, Timer};
-use fixed::traits::ToFixed; // For 5 - servo
-//use irqs::Irqs; // already defined
+use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
-use defmt::*; // Use the logging macros provided by defmt.
-
-// use embassy_rp::{config, peripherals};
-use embassy_rp::{adc::{Adc, InterruptHandler}, config, gpio::{AnyPin, Input, Level, Output, Pin, Pull}, pwm::{Pwm, SetDutyCycle}, spi::{self, Spi}};
-use embassy_rp::pwm::Config as ConfigPwm;
-use embassy_rp::adc::Config as ConfigAdc; // ADC config
-use embassy_rp::bind_interrupts;
-
-use embassy_sync::{blocking_mutex::raw::{CriticalSectionRawMutex, ThreadModeRawMutex}, channel::{Channel, Receiver, Sender}};
-use embassy_sync::signal::Signal;
-use embassy_futures::select::Either::Second;
-use embassy_futures::select::Either::First;
-use embassy_futures::join::join;
-
-
-// Import interrupts definition module
-mod irqs;
-
-// Import
-mod utils;
-use utils::{creeate_pwm_config, update_pwm_config, Lane};
-
-
-bind_interrupts!(struct Irqs {
-    ADC_IRQ_FIFO => InterruptHandler;
-});
-
-pub enum LedControl {
-    Increase,
-    Decrease,
-}
-
-static CHANNEL: Channel<ThreadModeRawMutex, LedControl, 64> = Channel::new();
-static SIG: Signal<CriticalSectionRawMutex, u16> = Signal::new();
-
+use defmt::*;
+use embassy_rp::gpio::{Output, Level};
+use embassy_rp::spi::{self, Spi};
+use embedded_hal_1::spi::SpiDevice as _;
+const WHO_AM_I: u8 = 0x75; // in decimal, 117 (0x75)
+const ACCEL_CONFIG: u8 = 0x1C; // in decimal, 28 (0x1C)
+const GYRO_CONFIG: u8 = 0x1B; // in decimal, 27 (0x1B)
+const ACCEL_XOUT_H: u8 = 0x43; // in decimal, 59 (0x3B)
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
-    info!("Hello world!");
-    const REG_ADDR: u8 = 0x75;
-
-    // Get a handle to the RP's peripherals.
-    let peripherals = embassy_rp::init(Default::default());
-    let mut adc = Adc::new(peripherals.ADC, Irqs, ConfigAdc::default());
-
-    // intializare
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    
     let mut config = spi::Config::default();
     config.frequency = 1_000_000;
     config.phase = spi::Phase::CaptureOnFirstTransition;
     config.polarity = spi::Polarity::IdleLow;
-
-    let mosi = peripherals.PIN_3;
-    let miso = peripherals.PIN_4;
-    let clk = peripherals.PIN_2;
-
-    let mut spi = Spi::new(peripherals.SPI0, clk, mosi, miso, peripherals.DMA_CH0, peripherals.DMA_CH1, config);
-    let mut cs = Output::new(peripherals.PIN_5, Level::High);
-
+    
+    let mosi = p.PIN_3; // MOSI (GPIO)
+    let miso = p.PIN_4; // MISO (GPIO)
+    let clk = p.PIN_2;  // CLK (GPIO)
+    
+    let mut spi = Spi::new(p.SPI0, clk, mosi, miso, p.DMA_CH0, p.DMA_CH1, config);
+    let mut cs = Output::new(p.PIN_5, Level::High);
+    
+    // Read WHO_AM_I register to check sensor communication
     cs.set_low();
-    let tx_buf = [(1 << 7) | REG_ADDR, 0x00]; // first value of buffer is the control byte, second is a *don't care* value
-    let mut rx_buf = [0u8; 2]; // initial values that will be replaced by the received bytes
+    let tx_buf = [(1 << 7) | WHO_AM_I, 0x00];
+    let mut rx_buf = [0u8; 2];
     spi.transfer(&mut rx_buf, &tx_buf).await.unwrap();
     cs.set_high();
-
-    let register_value = rx_buf[1]; // the second byte in the buffer will be the received register value
-    info!("{}", register_value);
-    //117
-    // loop {
-
-    // }
+    
+    info!("WHO_AM_I: {}", rx_buf[1]);
+    
+    // Configure Accelerometer (±2g)
+    cs.set_low();
+    let _ = spi.write(&[ACCEL_CONFIG, 0b00000000]).await;
+    cs.set_high();
+    
+    Timer::after(Duration::from_millis(10)).await;
+    
+    // Configure Gyroscope (±1000°/s)
+    cs.set_low();
+    let _ = spi.write(&[GYRO_CONFIG, 0b00010000]).await;
+    cs.set_high();
+    
+    Timer::after(Duration::from_millis(10)).await;
+    
+    // Read and print sensor values in a loop
+    loop {
+        cs.set_low();
+        let tx_buf = [(1 << 7) | ACCEL_XOUT_H];
+        let mut rx_buf = [0u8; 15]; // 1 extra byte for response alignment
+        spi.transfer(&mut rx_buf, &tx_buf).await.unwrap();
+        cs.set_high();
+        
+        let accel_x_raw = i16::from_be_bytes([rx_buf[1], rx_buf[2]]);
+        let accel_y_raw = i16::from_be_bytes([rx_buf[3], rx_buf[4]]);
+        let accel_z_raw = i16::from_be_bytes([rx_buf[5], rx_buf[6]]);
+        let gyro_x_raw = i16::from_be_bytes([rx_buf[9], rx_buf[10]]);
+        let gyro_y_raw = i16::from_be_bytes([rx_buf[11], rx_buf[12]]);
+        let gyro_z_raw = i16::from_be_bytes([rx_buf[13], rx_buf[14]]);
+        
+        // info!(
+        //     "Accel: X={}, Y={}, Z={} | Gyro: X={}, Y={}, Z={}",
+        //     accel_x_raw, accel_y_raw, accel_z_raw, gyro_x_raw, gyro_y_raw, gyro_z_raw
+        // );
+        
+        let accel_x = (accel_x_raw as f32 / 16384.0) * 9.80665;
+        let accel_y = (accel_y_raw as f32 / 16384.0) * 9.80665; 
+        let accel_z = (accel_z_raw as f32 / 16384.0) * 9.80665;
+        let gyro_x = gyro_x_raw as f32 / 32.8;
+        let gyro_y = gyro_y_raw as f32 / 32.8;
+        let gyro_z = gyro_z_raw as f32 / 32.8;
+        
+        info!(
+            "Accel: X={}, Y={}, Z={} m/s²  |  Gyro: X={}, Y={}, Z={} °/s",
+            accel_x, accel_y, accel_z , gyro_x, gyro_y, gyro_z
+        );
+        
+        Timer::after(Duration::from_secs(1)).await;
+    }
 }
